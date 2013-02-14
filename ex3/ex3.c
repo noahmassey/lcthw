37970c04403455a6cd32753c46208d1160f2f85f
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2013-02-14 14:25:11 -0500

    ex3 extra credit

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -4,9 +4,17 @@ int main()
 {
 	int age = 10;
 	int height = 73;
+	int foo = height + age;
+	const char * str = "I am a string and stuff";
 
 	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
+	printf("The value of foo is %d\n", foo);
+	printf("\a\n");
+	printf("vertical \v tab\n");
+	printf("args order? %2$d and %1$d\n", age, height);
+	printf("hex: %08x\n", age);
+	printf("%s\n", str);
 
 	return 0;
 }
