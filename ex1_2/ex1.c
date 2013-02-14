6b5d1c05f661fae2d158b946ae8205b54dc448ff
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2013-02-14 13:10:02 -0500

    ex1 & ex2 complete

---
+++ ex1_2/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("Foo");
+	puts("Bar");
+	puts("Something");
+	puts("Somthing");
+	puts("Goodbye world.");
+
+	return 0;
+}
