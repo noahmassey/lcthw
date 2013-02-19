77c933eaa27abad271c7174c5e04bbe7544d3fc4
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2013-02-19 14:16:14 -0500

    extra credit for ex12

--- ex12/ex12.c
+++ ex12/ex12.c
@@ -4,14 +4,16 @@ int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	if (argc == 1) {
+	if (argc == 2) {
 		printf("You only have one argument. You suck.\n");
-	} else if (argc > 1 && argc < 4) {
+	} else if (argc > 2 && argc < 5) {
 		printf("Here's your arguments:\n");
-		for (i = 0; i < argc; i++) {
+		for (i = 1; i < argc; i++) {
 			printf("%s ", argv[i]);
 		}
 		printf("\n");
+	} else if (argc > 10) {
+		printf("Whoa! %d is way to many args, calm down buddy.\n", argc);
 	} else {
 		printf("You have too many arguments. You suck.\n");
 	}
