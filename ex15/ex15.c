4cbe27d7407ea549a76aaa88860a60b50418127e
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2013-02-19 23:15:13 -0500

    a bit of extra credit for ex15

--- ex15/ex15.c
+++ ex15/ex15.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
+void for_loop(int count, int *ages, char **names)
+{
+	printf("---for_loop\n");
+	int i = 0;
+	for (i = 0; i < count; i++) {
+		printf("%s is %d years old.\n",
+				names[i], ages[i]);
+	}
+	printf("---\n");
+}
+
+void while_loop(int count, int *ages, char **names)
+{
+	printf("---while_loop\n");
+	int i = 0;
+	while (i < count) {
+		printf("%s is %d years old.\n",
+				names[i], ages[i]);
+		i++;
+	}
+}
+
+void get_addresses(int count, int *ages, char **names)
+{
+	printf("---get_addresses\n");
+	int i = 0;
+	for (i = 0; i < count; i++) {
+		printf("%s [address: %p] is %d [address: %p] years old.\n",
+				names[i], &names[i], ages[i], &ages[i]);
+	}
+	printf("---\n");
+}
+
 int main(int argc, char *argv[])
 {
 	// create two arrays we care about
@@ -12,50 +45,16 @@ int main(int argc, char *argv[])
 		"Lisa",
 	};
 
-	// safely get the size of ages
-	int count = sizeof(ages) / sizeof(int);
-	int i = 0;
-
-	// first way using indexing
-	for (i = 0; i < count; i++) {
-		printf("%s has %d years alive.\n",
-				names[i], ages[i]);
-	}
-	printf("---\n");
-
 	// setup the pointers to the start of the arrays
 	int *cur_age = ages;
 	char **cur_name = names;
 
-	// second way of using pointers
-	for (i = 0; i < count; i++) {
-		printf("%s is %d years old again.\n",
-				*(cur_name + i), *(cur_age + i));
-	}
-	printf("---\n");
+	// safely get the size of ages
+	int count = sizeof(ages) / sizeof(int);
 
-	// third way; pointers are just arrays
-	for (i = 0; i < count; i++) {
-		printf("%s is %d years old again.\n",
-				cur_name[i], cur_age[i]);
-	}
-	printf("---\n");
-
-	// fourth way with pointers in a stupid complex way
-	for (cur_name = names, cur_age = ages;
-			(cur_age - ages) < count;
-			cur_name++, cur_age++)
-	{
-		printf("%s lived %d years so far.\n",
-				*cur_name, *cur_age);
-	}
+	for_loop(count, cur_age, cur_name);
+	while_loop(count, cur_age, cur_name);
+	get_addresses(count, cur_age, cur_name);
 
 	return 0;
 }
-
-	//
-	// Noah
-	//while(count--) {
-	//	printf ("%s lived %d years so far.\n",
-	//			*cur_name++, *cur_age++);
-	//}
