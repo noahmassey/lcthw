e5b7e940490c55f09cf43c4811d094b4c02ff1cf
Noah Massey <nmassey@kmbs.konicaminolta.us>
2013-02-22 22:04:19 -0500

    Exercise 17 :: create database with arbitrary sizes

--- ex17/ex17.c
+++ ex17/ex17.c
@@ -10,12 +10,14 @@
 struct Address {
 	int id;
 	int set;
-	char name[MAX_DATA];
-	char email[MAX_DATA];
+	char *name;
+	char *email;
 };
 
 struct Database {
-	struct Address rows[MAX_ROWS];
+	int max_data;
+	int max_rows;
+	struct Address *rows;
 };
 
 struct Connection {
@@ -94,11 +96,15 @@ void Database_write(struct Connection *conn)
 	if (rc == -1) die(conn, "Cannot flush database");
 }
 
-void Database_create(struct Connection *conn)
+void Database_create(struct Connection *conn, int max_rows, int max_data)
 {
 	int i = 0;
+	struct Database * db = conn->db;
 
-	for (i = 0; i < MAX_ROWS; i++) {
+	db->max_rows = max_rows;
+	db->max_data = max_data;
+	db->rows = malloc(sizeof(struct Address) * max_rows);
+	for (i = 0; i < max_rows; i++) {
 		// make a prototype to initialize it
 		struct Address addr = { .id = i, .set = 0 };
 		
@@ -109,30 +115,30 @@ void Database_create(struct Connection *conn)
 
 void Database_set(struct Connection *conn, int id, const char *name, const char *email)
 {
-	struct Address *addr = &conn->db->rows[id];
+	struct Address *addr;
+
+	if (!(id < conn->db->max_rows))
+		die(conn, "There aren't that many records");
+	addr = &conn->db->rows[id];
 	if (addr->set)
 		die(conn, "Already set, delete it first");
 
 	addr->set = 1;
 
-	// WARNING: bug, read the "How To Break It" and fix this
-	char *res = strncpy(addr->name, name, MAX_DATA);
-
-	// demonstrate the strncpy bug
-	//if (!res)
-	//	die("Name copy failed");
-	
-	// Fix the strncpy bug
-	res[MAX_DATA - 1] = '\0';
-
-	res = strncpy(addr->email, email, MAX_DATA);
-	if (!res)
-		die(conn, "Email copy failed");
+	addr->name = strndup(name, conn->db->max_data);
+	addr->email = strndup(email, conn->db->max_data);
+	if (!(addr->name && addr->email))
+		die(conn, "Out of memory");
 }
 
 void Database_get(struct Connection *conn, int id)
 {
-	struct Address *addr = &conn->db->rows[id];
+	struct Address *addr;
+
+	if (!(id < conn->db->max_rows))
+		die(conn, "There aren't that many records");
+
+	addr = &conn->db->rows[id];
 
 	if (addr->set) {
 		Address_print(addr);
@@ -143,18 +149,29 @@ void Database_get(struct Connection *conn, int id)
 
 void Database_delete(struct Connection *conn, int id)
 {
-	struct Address addr = { .id = id, .set = 0 };
-	conn->db->rows[id] = addr;
+	struct Address *addr;
+
+	if (!(id < conn->db->max_rows))
+		die(conn, "There aren't that many records");
+	addr = &conn->db->rows[id];
+	addr->set = 0;
+	if (addr->name) {
+		free(addr->name);
+		addr->name = NULL;
+	}
+	if (addr->email) {
+		free(addr->email);
+		addr->email = NULL;
+	}
 }
 
 void Database_list(struct Connection *conn)
 {
-	int i = 0;
+	int i;
 	struct Database *db = conn->db;
+	struct Address *cur;
 
-	for (i = 0; i < MAX_ROWS; i++) {
-		struct Address *cur = &db->rows[i];
-
+	for (i = db->max_rows, cur = db->rows; i != 0; i--, cur++) {
 		if (cur->set) {
 			Address_print(cur);
 		}
@@ -168,33 +185,30 @@ int main(int argc, char *argv[])
 	char *filename = argv[1];
 	char action = argv[2][0];
 	struct Connection *conn = Database_open(filename, action);
-	int id = 0;
-
-	if (argc > 3)
-		id = atoi(argv[3]);
-	if (id >= MAX_ROWS)
-		die(conn, "There aren't that many records");
 
 	switch (action) {
 		case 'c':
-			Database_create(conn);
+			if (argc != 5)
+				die(conn, "Need to specify total entries and  max length");
+
+			Database_create(conn, atoi(argv[3]), atoi(argv[4]));
 			Database_write(conn);
 			break;
 		case 'g':
 			if (argc != 4) die(conn, "Need an id to get");
 
-			Database_get(conn, id);
+			Database_get(conn, atoi(argv[3]));
 			break;
 		case 's':
 			if (argc != 6)
 				die(conn, "Need id, name, email to set");
-			Database_set(conn, id, argv[4], argv[5]);
+			Database_set(conn, atoi(argv[3]), argv[4], argv[5]);
 			Database_write(conn);
 			break;
 		case 'd':
 			if (argc != 4)
 				die(conn, "Need an id to delete");
-			Database_delete(conn, id);
+			Database_delete(conn, atoi(argv[3]));
 			Database_write(conn);
 			break;
 		case 'l':
